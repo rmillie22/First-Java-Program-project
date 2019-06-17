153eb5ad35fbd4b09b45e5637c7806d6aed9a124
Milagros D Rodriguez <50366134+rmillie22@users.noreply.github.com>
2019-06-17 12:48:10 -0500

    Add files via upload

---
+++ First Java Program project.java
@@ -0,0 +1,7 @@
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Milagros Rodriguez");
+        System.out.println("I am 32 years old");
+        System.out.println("Im from Brooklyn, NY");
+    }
+}
